a0682730e82ac1bb216532c0ed1d2b0c6dd0700b
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2021-08-11 22:14:42 -0400

    created React hook for MintGate link creation API

---
+++ src/hooks/createMintGate.js
@@ -0,0 +1,42 @@
+import {useEffect} from 'react';
+
+const createMintGate = (url, title, token_address, balance, jwt) => {
+    useEffect(() => {
+        const linkOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "url": url,
+                "title": title,
+                "tokens": [
+                  {
+                    "token": token_address,
+                    "ttype": "20",
+                    "balance": balance,
+                    "network": 1,
+                    "subid": "",
+                  }
+                ],
+                "jwt": jwt
+            })
+            };
+            fetch('https://mgate.io/api/v2/links/create', linkOptions)
+            .then(response => response.json())
+            .then((data) => {
+                if (data && (data.status === 'fail')) {
+                  let msg = data;
+                  alert("Form Error: " + msg);
+                  console.error(msg);
+                  return;
+                }
+                return data.url;
+              })
+              .catch((e) => {
+                alert("Oh no! We have an error: " + e.toString());
+                console.error(e);
+              });
+    }, [])
+}
+
+export {createMintGate};
