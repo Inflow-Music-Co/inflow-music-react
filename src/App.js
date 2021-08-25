02be336f07afd1194969941e51601afb3137d9ea
derivativedegen <derivativedegen@gmail.com>
2021-08-25 16:59:06 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@
 import React, { useState } from "react";
 import { AppRoutes } from "./route/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
-import './utils/axios'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import "./utils/axios";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // const { store, persistor } = configureStore();
-import Header from '../src/base/Header';
-import Sidebar from '../src/base/Sidebar';
+import Header from "../src/base/Header";
+import Sidebar from "../src/base/Sidebar";
 import { WalletProviderContext } from "./contexts/walletProviderContext";
-
 
 function App() {
   const [walletProvider, setWalletProvider] = useState(null);
 
   // const clienturl = useSelector((state) => state.graphql.clienturl);
-  const clienturl = 'https://api.studio.thegraph.com/query/6287/inflow-rinkeby/0.0.1';
+  const clienturl =
+    "https://api.studio.thegraph.com/query/6287/inflow-rinkeby/0.0.1";
 
   const client = new ApolloClient({
     uri: clienturl,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
 
   return (
-    <WalletProviderContext.Provider value={{ walletProvider, setWalletProvider }}>
+    <WalletProviderContext.Provider
+      value={{ walletProvider, setWalletProvider }}
+    >
       <ApolloProvider client={client}>
         <Router>
           <AppRoutes />
@@ -44,4 +42,3 @@ function App() {
 }
 
 export default App;
-
