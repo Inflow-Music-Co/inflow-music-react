b026c9033170fc5a10f8f89e514dfea32e55af66
Richard Melkonian <r.v.melkonian@gmail.com>
2021-08-27 11:36:33 +0100

    'Merge' Header.js from 'degen'

--- src/base/Header.js
+++ src/base/Header.js
@@ -1,93 +1,106 @@
-import React, { useContext, useState, useEffect } from 'react';
-import './base.css';
+import React, { useContext, useState, useEffect } from "react";
+import "./base.css";
 //import Search from '../component/Search';
-import Notification from '../component/Notification';
-import Profiledropdown from '../component/Profiledropdown';
+import Notification from "../component/Notification";
+import Profiledropdown from "../component/Profiledropdown";
 import { Button } from "react-bootstrap";
-import Wallet from '../utils/wallet';
+import Wallet from "../utils/wallet";
 import SweetAlert from "react-bootstrap-sweetalert";
 import { useDispatch, useSelector } from "react-redux";
 import { connected, disconnect } from "../store/reducers/walletSlice";
-import { WalletProviderContext } from '../contexts/walletProviderContext';
+import { WalletProviderContext } from "../contexts/walletProviderContext";
+import LoginModal from "../page/LoginModal";
 
 const Header = () => {
-    const { walletProvider, setWalletProvider } = useContext(WalletProviderContext);
-    const wallet = useSelector(state => state.wallet);
-    const token = useSelector(state => state.auth.token);
-    // const wallet = useSelector(state => state.wallet);
-    const dispatch = useDispatch()
-    const [alert, setalert] = useState(null);
+  const { walletProvider, setWalletProvider } = useContext(
+    WalletProviderContext
+  );
+  const wallet = useSelector((state) => state.wallet);
+  const token = useSelector((state) => state.auth.token);
+  // const wallet = useSelector(state => state.wallet);
+  const dispatch = useDispatch();
+  const [alert, setalert] = useState(null);
+  const [login, setLogin] = useState(false);
 
-    // console.log({wallet})
-    useEffect(() => {
-        if (wallet.wallet_connected) {
-            conn();
+  // console.log({wallet})
+  useEffect(() => {
+    if (wallet.wallet_connected) {
+      conn();
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  const conn = async () => {
+    await Wallet.connect();
+    dispatch(connected({ address: Wallet.account }));
+    setWalletProvider(Wallet.ethersProvider);
+  };
+
+  const connectWallet = async () => {
+    if (!token) {
+      window.location.href = "/login";
+      return;
+    }
+    try {
+      if (!walletProvider) {
+        if (!Wallet.ethersProvider) {
+          await Wallet.connect();
         }
-        // eslint-disable-next-line
-    }, [])
-
-    const conn = async () => {
-        await Wallet.connect();
         dispatch(connected({ address: Wallet.account }));
-        setWalletProvider(Wallet.ethersProvider)
+        setWalletProvider(Wallet.ethersProvider);
+        showAlert("Wallet connected successfully", "success");
+        // setTimeout(() => {
+        //     window.location.reload();
+        // },1500)
+      } else {
+        Wallet.disconnect(true);
+        showAlert("Wallet disconnected", "info");
+        dispatch(disconnect());
+        setWalletProvider(null);
+      }
+    } catch (e) {
+      // console.log(e);
     }
+  };
 
-    const connectWallet = async () => {
-        if (!token) {
-            window.location.href = "/login"
-            return;
-        }
-        try {
-            if (!walletProvider) {
-                if (!Wallet.ethersProvider) {
-                    await Wallet.connect();
-                }
-                dispatch(connected({ address: Wallet.account }));
-                setWalletProvider(Wallet.ethersProvider)
-                showAlert('Wallet connected successfully', 'success');
-                // setTimeout(() => {
-                //     window.location.reload();
-                // },1500)
-            } else {
-                Wallet.disconnect(true);
-                showAlert('Wallet disconnected', 'info');
-                dispatch(disconnect());
-                setWalletProvider(null)
-            }
-        } catch (e) {
-            // console.log(e);
-        }
-    }
+  const showAlert = (title, type) => {
+    setalert(
+      <SweetAlert
+        style={{ color: "#000" }}
+        type={type}
+        onConfirm={hideAlert}
+        timeout={2000}
+        title={title}
+      />
+    );
+  };
 
-    const showAlert = (title, type) => {
-        setalert(<SweetAlert style={{ color: '#000' }} type={type} onConfirm={hideAlert} timeout={2000} title={title} />)
-    }
+  const hideAlert = () => {
+    setalert(null);
+  };
 
-    const hideAlert = () => {
-        setalert(null);
-    }
-
-    return (
-        <div className="header-main">
-            {alert}
-            <div className="left-col-main">
-                {/* <Search /> */}
-            </div>
-            <div className="right-col-main">
-                <Button size="sm" className="mr-2 wallet-button" onClick={() => connectWallet()}>
-                    {walletProvider ? 'Disconnect Wallet' : 'Connect Wallet'}
-                </Button>
-                <div className="notified-main">
-                        <Notification />
-                    </div>
-                <div className="profile-dropdown">
-                    <Profiledropdown />
-                </div>
-            </div>
+  return (
+    <div className="header-main">
+      {alert}
+      <div className="left-col-main">{/* <Search /> */}</div>
+      <div className="right-col-main">
+        <Button
+          size="sm"
+          className="mr-2 wallet-button"
+          //   onClick={() => connectWallet()}
+          onClick={() => setLogin((login) => !login)}>
+          {walletProvider ? "Logout" : "Login"}
+        </Button>
+        <div className="notified-main">
+          <Notification />
         </div>
-    )
-}
-
-
+        <div className="profile-dropdown">
+          <Profiledropdown />
+        </div>
+      </div>
+      <LoginModal login={login} setLogin={setLogin}/>
+    </div>
+  );
+};
 
 export default Header;
