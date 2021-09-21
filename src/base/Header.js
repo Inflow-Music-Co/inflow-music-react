ae79cc9d8bc23ed9479a884dac3a5d9cecc2f1e1
Richard Melkonian <r.v.melkonian@gmail.com>
2021-09-21 14:39:11 +0100

    created new My Dashboard Button / Login Button in Header

--- src/base/Header.js
+++ src/base/Header.js
@@ -11,13 +11,38 @@ import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { updateActivePage } from "../store/reducers/appSlice";
 import { logout } from "../store/reducers/authSlice";
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button';
+import LoginModal from '../page/LoginModal'
+
+const styles = {
+  button: { 
+      color: 'white',
+      background: 'linear-gradient(45deg, #8A17A5 30%, #707DF8 90%)',
+      borderRadius: 30,
+      boxShadow: '20px',
+      '&:hover': {
+        background: 'linear-gradient(45deg, #707DF8 30%, #8A17A5 90%)'       
+      },
+    }
+};
+
+const useStyles = makeStyles(styles);
 
 const Header = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const MySwal = withReactContent(Swal);
-  const wallet = useSelector((state) => state.wallet);
+  
   const token = useSelector((state) => state.auth.token);
+  const classes = useStyles();
+  const [login, setLogin] = useState(false);
+  const dispatch = useDispatch();
+
+  const updatePage = (page) => {
+    dispatch(updateActivePage(page));
+  };
+
+  const onLogin = () => {
+    setLogin((login) => !login);
+  };
 
   return (
     <div className="header-main">
@@ -34,10 +59,27 @@ const Header = () => {
         </Link>
       </div>
       <div className="left-col-main">{/* <Search /> */}</div>
-
+      {token ? 
+        <Link to={'/dashboard'}>
+        <Button 
+          className={classes.button} 
+          variant="contained" 
+          size="large" 
+          color="secondary" 
+          style={{marginLeft: 10}}
+          onClick={() => updateActivePage("dashboard")}>MY DASHBOARD</Button>
+        </Link>
+       : <Button 
+          className={classes.button} 
+          variant="contained" 
+          size="large" 
+          color="secondary" 
+          style={{marginLeft: 10}}
+          onClick={onLogin}>LOGIN</Button>}
       <div className="right-col-main">
-        
+      <LoginModal login={login} setLogin={setLogin} />
         <div className="profile-dropdown">
+          
           <Profiledropdown />
         </div>
       </div>
